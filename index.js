b92c89e09cd24d07c4c3dbc232f40f16b2555d69
nishat1 <nishatgupta1@gmail.com>
2017-12-29 21:15:23 -0800

    modified calculator

--- index.js
+++ index.js
@@ -13,6 +13,13 @@ const buysellRadioBtn = document.getElementsByName('buysell');
 const buySellAmount = document.getElementById('buySellAmount');
 const bitcoinAmount = document.getElementById('bitcoinAmount');
 
+const dollarInput = document.getElementById('dollarInput');
+const cryptoInput = document.getElementById('cryptoInput');
+const calculateBtn = document.getElementById('calculateBtn');
+const calculatedAmount = document.getElementById('calculatedAmount');
+const calculatedAmountBox2 = document.getElementById('calculatedAmountBox2');
+const calculationType = document.getElementsByName('calculationType');
+
 // Create references
 const dbConvRateObj = firebase.database().ref().child('conv_rates');
 
@@ -127,20 +134,25 @@ function calculateCoins() {
   var bitcoinAmountVal = bitcoinAmount.value;
   var coinVal;
   var coinType;
+  var txFee;
 
   if(buysellRadioBtn[0].checked) {
     if(currencyRadioBtn[0].checked) {
       coinVal = btcValBuy;
       coinType = " Bitcoin";
+      txFee = "0.0025 BTC";
     } else if(currencyRadioBtn[1].checked) {
       coinVal = bchValBuy;
       coinType = " Bitcoin Cash";
+      txFee = "0.001 BCH";
     } else if(currencyRadioBtn[2].checked) {
       coinVal = ethValBuy;
       coinType = " Ethereum";
+      txFee = "0.01 ETH";
     } else if(currencyRadioBtn[3].checked) {
       coinVal = ltcValBuy;
       coinType = " Litecoin";
+      txFee = "N/A";
     }
   } else if(buysellRadioBtn[1].checked) {
     if(currencyRadioBtn[0].checked) {
@@ -158,9 +170,27 @@ function calculateCoins() {
     }
   }
 
-  document.getElementById('calculatedAmount').innerHTML = "<b>Number of coins:</b> " + amountVal/coinVal
-    + coinType + " @ $" + coinVal + "/" + coinType;
+  calculatedAmount.innerHTML = "<b>Number of coins:</b> " + amountVal/coinVal
+    + coinType + " @ $" + coinVal + "/" + coinType
+    + "<br><b>TX Fee:</b> " + txFee;
 
-  document.getElementById('calculatedAmountBox2').innerHTML = "<b>Cost of coins:</b> &emsp;&nbsp;&nbsp;" + "$" + parseFloat(Math.round(bitcoinAmountVal*coinVal*100)/100).toFixed(2)
-    + " for " + bitcoinAmountVal + " " + coinType;
+  calculatedAmountBox2.innerHTML = "<b>Cost of coins:</b> " + "$" + parseFloat(Math.round(bitcoinAmountVal*coinVal*100)/100).toFixed(2)
+    + " for " + bitcoinAmountVal + " " + coinType
+    + "<br><b>TX Fee:</b> " + txFee;
+}
+
+function calculationDollarType() {
+  dollarInput.classList.remove('hide');
+  cryptoInput.classList.add('hide');
+  calculateBtn.classList.remove('hide');
+  calculatedAmount.classList.remove('hide');
+  calculatedAmountBox2.classList.add('hide');
+}
+
+function calculationCryptoType() {
+  dollarInput.classList.add('hide');
+  cryptoInput.classList.remove('hide');
+  calculateBtn.classList.remove('hide');
+  calculatedAmount.classList.add('hide');
+  calculatedAmountBox2.classList.remove('hide');
 }
